8b54d2f37e51dca8ca04d442b80a099c7fec23c6
Christian C <cc@localhost>
2025-03-22 20:45:56 -0700

    Finetune information

--- code/sunlab/sunflow/models/__init__.py
+++ code/sunlab/sunflow/models/__init__.py
@@ -4,4 +4,4 @@ from sunlab.common.data.dataset import Dataset
 from sunlab.common.distribution.adversarial_distribution import AdversarialDistribution
 from sunlab.common.scaler.adversarial_scaler import AdversarialScaler
 from .utilities import create_aae, create_aae_and_dataset
-from .utilities import load_aae, load_aae_and_dataset
+from .utilities import load_aae, load_aae_and_dataset, load_aae_finetune
